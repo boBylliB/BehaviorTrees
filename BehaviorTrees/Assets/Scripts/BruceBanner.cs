c50c9c46c87318b16419081e952052877e5bc1c8
Samuel Sandelin <jrsandelin@gmail.com>
2024-03-29 00:18:34 -0700

    Adding a reset button

--- BehaviorTrees/Assets/Scripts/BruceBanner.cs
+++ BehaviorTrees/Assets/Scripts/BruceBanner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BruceBanner : Kinematic
 {
@@ -32,6 +33,8 @@ public class BruceBanner : Kinematic
             running = true;
             taskList.run();
         }
+        if (Input.GetKeyDown("r"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         steeringUpdate = new SteeringOutput();
         steeringUpdate.linear = myMoveType.getSteering().linear;
         base.Update();
